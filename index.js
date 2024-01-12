431ccc0745412444b6a5c213b5a0e38b595823f2
Ubuntu <ubuntu@ip-172-31-86-92.ec2.internal>
2024-01-12 13:26:34 +0000

    Your commit message-frist commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello, this is a simple Node.js web application!');
+});
+
+const port = 3000;
+const server = app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
+});
